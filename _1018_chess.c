6988542fbd862a951559a9771f8b2a6fd389009f
박정욱 <jupark0369@gmail.com>
2023-03-29 18:05:32 +0900

    hanoi_tower

--- _1018_chess.c
+++ _1018_chess.c
@@ -9,5 +9,16 @@ int main()
   for (int i = 0; i < N; i++)
   {
     scanf("%s", chess[i]);
-  }
+  } 
+  // char chess[50];
+  // scanf("%s", chess);
+
+  printf("\n\n\n");
+  printf("%s\n\n", chess[0]);
+  printf("%s\n\n", *chess);
+  printf("%s\n\n", chess[1]);
+  // printf("%s\n\n", *(++chess);
+  printf("%s\n\n", *(chess + 1));
+  printf("%u\n\n`", &chess[0]);
+  printf("%c\n\n", chess[0]);
 }
