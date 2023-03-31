5d86216ff6f0c4327346ee52402cc7afdf9d6fff
박정욱 <jupark0369@gmail.com>
2023-03-31 21:10:21 +0900

    bakjoon beeeeeeeee

--- _1181_wordSorting2.c
+++ _1181_wordSorting2.c
@@ -4,9 +4,10 @@
 
 #define col 50
 
-int compare(const void*first, const void*second){
-  if(*(int*)first>*(int*)second)
-  return 1;
+int compare(const void *first, const void *second)
+{
+  if (*(int *)first > *(int *)second)
+    return 1;
 }
 
 int main()
@@ -14,11 +15,14 @@ int main()
   int N;
   double K;
   scanf("%d", &N);
-  char str[N][col];
-  char result[N][col];
+  char str[][];
+  char **p;
+  str char result[N][col];
   char temp[col];
+
   for (int i = 0; i < N; i++)
     scanf("%s", str[i]);
 
-
+  qsort(str, N, sizeof(str[0]), compare);
+  for (int i = 0; i < N; i++)
 }
