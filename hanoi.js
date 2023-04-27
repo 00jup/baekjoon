6d9469c94421122bb57cf7c6e568f270e517960c
00jup <jupark0369@gmail.com>
2023-04-27 22:24:50 +0900

    드디어 이해함

--- hanoi.js
+++ hanoi.js
@@ -7,15 +7,22 @@ let rl = readline.createInterface({
 function hanoi_t(N, from, tmp, to) {
   if (N >= 1) {
     hanoi_t(N - 1, from, to, tmp);
-    console.log(from + "를 " + to + "로 이동");
+    console.log(from + " " + to);
     hanoi_t(N - 1, tmp, from, to);
   }
   return;
 }
+function cnt(N) {
+  if (N == 1) return 1;
+  else return 2 ** N - 1;
+}
 
 let N;
+let h_cnt = 0;
 rl.on("line", function (line) {
   N = Number(line);
-  hanoi_t(N, "A", "B", "C");
+  h_cnt = cnt(N);
+  console.log(h_cnt);
+  hanoi_t(N, "1", "2", "3");
   rl.close();
 });
