860e795a2456423625d67e73152cef490897ae81
00jup <jupark0369@gmail.com>
2023-04-27 17:37:07 +0900

    javascript hanoi tower

--- hanoi_t.js
+++ hanoi_t.js
@@ -4,20 +4,17 @@ let rl = readline.createInterface({
   output: process.stdout,
 });
 let N;
-rl.on("line", (line) => {
+rl.on("line", function (line) {
   N = Number(line);
-  console.log(N);
 });
+rl.close();
 
-
-const solution = hanoi_t(N, from, tmp, to) {
-  const num =(N + "").split(" ").map((s) => +s);
-  const [from, tmp, to] = [num[0], num[1], num[2]];
+function hanoi_t(N, from, tmp, to) {
   if (N >= 1) {
     hanoi_t(N - 1, from, to, tmp);
     console.log(from + "를 " + to + "로 이동");
     hanoi_t(N - 1, tmp, from, to);
   }
   return;
-};
-process.stdin.on("number", solution);
+}
+hanoi_t(N, "A", "B", "C");
