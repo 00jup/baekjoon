201cec094b9300c36cb870e0c9e385a93a29e0dd
00jup <jupark0369@gmail.com>
2023-05-04 10:59:17 +0900

    string typeconversion 1436.py

---
+++ _1436.py
@@ -0,0 +1,9 @@
+N = int(input())
+number = 0
+count = 0
+while count < N :
+  number += 1
+  if '666' in str(number):
+    count += 1
+
+print(number)
--- _2839_2.py
+++ _2839_2.py
@@ -2,11 +2,14 @@ import sys
 N = int(sys.stdin.readline())
 count = 0
 while N>=0:
+  if N==1:
+    count = -1
+    break
   if(N%5==0):
-    count = N / 5
+    count = count + N / 5
     break
   count += 1
   N -= 3
   
-print(count)
+print(int(count))
 
