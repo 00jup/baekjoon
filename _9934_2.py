8aa221dd90a19f048404bf9150a30bc0750d772f
00jup <jupark0369@gmail.com>
2023-05-06 01:04:18 +0900

    inorder, recursion problem

---
+++ _9934_2.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+b = input().strip().split(' ')
+
+tree = {}
+
+for i in range(N):
+  root, left, right = [i, 0, 0]
+  tree[root]=[left, right]
+  i += 1
+
+i = 0
+
+def inorderTraveral(root):
+  inorderTraveral(tree[root][0])
+  tree[b[i]] = tree[root]
+  del tree[root]
+  i += 1
+  inorderTraveral(tree[root][1])
+
+inorderTraveral(0)
+
+print(tree)
