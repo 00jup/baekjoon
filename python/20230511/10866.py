f44a9c2dde6964c378a61bf97e9bb95fe5f86a6e
00jup <jupark0369@gmail.com>
2023-05-11 16:39:05 +0900

    deque with python

---
+++ python/20230511/10866.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+deq = deque()
+N = int(input())
+
+for _ in range(N):
+    ans = input().split()
+    if ans[0] == 'push_front':
+        deq.appendleft(ans[1])
+    elif ans[0] == 'push_back':
+        deq.append(ans[1])
+    elif ans[0] == 'pop_front':
+        if len(deq) == 0:
+            print(-1)
+        else:
+            print(deq.popleft())
+    elif ans[0] == 'pop_back':
+        if len(deq) == 0:
+            print(-1)
+        else:
+            print(deq.pop())
+    elif ans[0] == 'size':
+        print(len(deq))
+    elif ans[0] == 'empty':
+        if len(deq) == 0:
+            print(1)
+        else:
+            print(0)
+    elif ans[0] == 'front':
+        print(deq[0])
+    elif ans[0] == 'back':
+        print(deq[-1])
