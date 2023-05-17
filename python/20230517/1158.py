8f62e0ad3ab5141f790201292683becd58027baf
00jup <jupark0369@gmail.com>
2023-05-18 08:37:09 +0900

    queue -> deque

--- python/20230517/1158.py
+++ python/20230517/1158.py
@@ -1,9 +1,19 @@
 import sys
-import queue
-
+from collections import deque
 
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-arr = queue.Queue()
+deq = deque()
+for i in range(n):
+    deq.append(i)
+
+while (True):
+    if arr.q == 0:
+        break
+    for _ in range(k):
+        tmp = deq.pop()
+        deq.pop()
+        deq.append(tmp)
+    print(deq.pop)
