00f53c48504eb3bab0204a4f5bf0a5eba3253c3d
00jup <jupark0369@gmail.com>
2023-05-22 11:40:32 +0900

    run time error

--- python/20230522/1389_3.py
+++ python/20230522/1389_3.py
@@ -21,9 +21,6 @@ for k in range(1, m+1):
             if graph[i][j] > graph[i][k]+graph[k][j]:
                 graph[i][j] = graph[i][k]+graph[k][j]
 # 옆에 초록 선은 뭐지
-for i in graph:
-    print(i)
-
 sum = [0] * (n+1)
 result = inf
 
@@ -31,8 +28,10 @@ for i in range(1, n+1):
     for j in range(1, n+1):
         sum[i] += graph[i][j]
 
-for i in range(1, n+1):
-    if result > sum[i] and sum[i] != 0:
-        result = sum[i]
 
-print(result)
+for i in range(1, n+1):
+    if result > sum[i] and sum[i] != 0 and sum[i] != result:
+        result = sum[i]
+        idx = i
+
+print(idx)
