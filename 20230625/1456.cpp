fe03653c9b9d899614f0e2993257f18b049d83b1
00jup <jupark0369@gmail.com>
2023-06-25 11:27:38 +0900

    completed 1456 average

--- 20230625/1456.cpp
+++ 20230625/1456.cpp
@@ -10,13 +10,18 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> arr[i];
 
-    int max_num;
-    max_num = max(arr[0], arr[n - 1]);
-
+    int max_num = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i + 1] > max_num)
+        {
+            max_num = arr[i + 1];
+        }
+    }
     double sum = 0;
 
     for (int i = 0; i < n; i++)
         sum += (arr[i] / max_num * 100);
 
-    cout << sum / n << endl;
+    cout << sum / double(n) << endl;
 }
